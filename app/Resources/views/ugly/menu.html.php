9567ec314e345f60f884b77c382ae66303b525a0
prunk.eger.edgar <prunk.eger.edgar@virgo.hu>
2018-05-05 11:03:48 +0200

    Some CSS

--- app/Resources/views/ugly/menu.html.php
+++ app/Resources/views/ugly/menu.html.php
@@ -1,3 +1,16 @@
+<style>
+    form div {
+        margin: 1em 0;
+    }
+
+    form div ul li {
+        color: red;
+    }
+
+    label {
+        display: block;
+    }
+</style>
 
 <a href="/">Saját címem</a> |
 <a href="/addresses">Barátnőm adatai</a>
